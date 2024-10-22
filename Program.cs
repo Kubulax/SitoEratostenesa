052b5d09af66b8e3c911e97b6e67ab0743451e75
jakub.koza <kubulax25@gmail.com>
2024-10-22 10:45:06 +0200

    Zaczęto prace nad programem

--- Program.cs
+++ Program.cs
@@ -4,11 +4,35 @@
     {
         static void Main(string[] args)
         {
-            int[] arrayOfNumbers = new int[101];
+            //n = 100
+            bool[] A = new bool [101];
 
-            for (int i = 0; i < arrayOfNumbers.Length; i++)
+            for (int i = 2; i < A.Length; i++)
             {
-                if ()
+                A[i] = true;
+            }
+
+            for (int i = 2; i < Math.Sqrt(A.Length - 1); i++)
+            {
+                if (A[i] == true)
+                {
+                    for (int j = 2; j < A.Length - 1; j++, j*=i)
+                    {
+                        A[j] = false;
+                    }
+                }
+            }
+
+            for (int i = 2; i < A.Length; i++)
+            {
+                if (A[i] == true)
+                {
+                    Console.WriteLine(i + ": True");
+                }
+                else
+                {
+                    Console.WriteLine(i + ": False");
+                }
             }
         }
     }
