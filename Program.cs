ec981f66fd2bab989c3c53f80ea585b357c5d2eb
jakub.koza <kubulax25@gmail.com>
2024-10-22 10:12:40 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SitoEratostenesa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOfNumbers = new int[101];
+
+            for (int i = 0; i < arrayOfNumbers.Length; i++)
+            {
+                if ()
+            }
+        }
+    }
+}
